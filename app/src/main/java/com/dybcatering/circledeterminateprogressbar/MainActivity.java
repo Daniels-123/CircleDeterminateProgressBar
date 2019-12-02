c6e1f62583c20eb9ebcef794638e2ad91b4b649a
Daniel Buitrago <dbuitragosa01@gmail.com>
2019-12-02 11:06:44 -0500

    Final commit

---
+++ app/src/main/java/com/dybcatering/circledeterminateprogressbar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.dybcatering.circledeterminateprogressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+	private ProgressBar progressBar;
+	private TextView textView;
+	private SeekBar seekBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		textView = findViewById(R.id.textview);
+		progressBar = findViewById(R.id.progressbar);
+		seekBar = findViewById(R.id.seekBar);
+
+
+		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				progressBar.setProgress(progress);
+				textView.setText("" + progress + "%");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+
+			}
+		});
+
+
+	}
+}
